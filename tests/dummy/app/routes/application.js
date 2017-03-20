a2ae3114f20544ca29706b6d68fdb30a529cc6eb
Deniss Alimovs <deniss.alimovs@gmail.com>
2017-03-20 16:16:46 +1100

    upgrade to latest everything

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 const a = Ember.A;
 
 export default Ember.Route.extend({
-  
+
   model() {
     return {
       one: a(['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco']),
@@ -12,21 +12,21 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    
+
     add(item, source, index, unique) {
-    	console.log('add item to:', unique);
-    	source.insertAt(index, item);
+      console.log('add item to:', unique);
+      source.insertAt(index, item);
     },
-    
+
     remove(item, source, index, unique) {
-    	console.log('remove item from:', unique);
-    	source.removeObject(item);
+      console.log('remove item from:', unique);
+      source.removeObject(item);
     },
-    
+
     move(item, source, index, unique) {
-    	console.log('move item within:', unique);
-    	source.removeObject(item).insertAt(index, item);
+      console.log('move item within:', unique);
+      source.removeObject(item).insertAt(index, item);
     }
-    
+
   }
 });
